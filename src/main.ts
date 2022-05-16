550bc1bdac23a09ead52498d4a663f705c53850b
Henrik Sørensen <hhs19942@gmail.com>
2022-05-16 17:12:26 +0200

    Set up Sentry

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue'
+import * as Sentry from "@sentry/vue";
+import App from "@/App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+const dsn = 'https://bd84d092c35549a28d0470b3be99868b@o1249239.ingest.sentry.io/6409646';
+const release = process.env.SENTRY_RELEASE;
+const options: Record<string, string | number> = {
+    dsn,
+    release,
+    tracesSampleRate: 1.0,
+};
+
+if (release === 'dev') {
+    options['environment'] = 'dev';
+}
+
+Sentry.init(options);
+
+app.mount('#app')
