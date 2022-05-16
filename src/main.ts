65d0eb82e9a88336d396a13b822a392db7f9ba03
Henrik Sørensen <hhs19942@gmail.com>
2022-05-16 18:02:03 +0200

    Use correct env var for sentry release

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import App from "@/App.vue";
 const app = createApp(App);
 
 const dsn = 'https://bd84d092c35549a28d0470b3be99868b@o1249239.ingest.sentry.io/6409646';
-const release = process.env.SENTRY_RELEASE;
+const release = process.env.VUE_APP_SENTRY_RELEASE;
 const options: Record<string, string | number> = {
     dsn,
     release,
