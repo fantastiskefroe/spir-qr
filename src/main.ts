bc824e99070fa4b0afeb5fb657eecb60d9843411
Henrik Sørensen <hhs19942@gmail.com>
2022-05-31 12:36:34 +0200

    Don't use sentry if release env var isn't available

--- src/main.ts
+++ src/main.ts
@@ -5,17 +5,19 @@ import App from "@/App.vue";
 const app = createApp(App);
 
 const dsn = 'https://bd84d092c35549a28d0470b3be99868b@o1249239.ingest.sentry.io/6409646';
-const release = process.env.VUE_APP_SENTRY_RELEASE;
 const options: Record<string, string | number> = {
     dsn,
-    release,
     tracesSampleRate: 1.0,
 };
 
+const release = process.env.VUE_APP_SENTRY_RELEASE;
 if (release === 'dev') {
+    options['release'] = 'dev';
     options['environment'] = 'dev';
 }
 
-Sentry.init(options);
+if (release !== undefined) {
+    Sentry.init(options);
+}
 
 app.mount('#app')
